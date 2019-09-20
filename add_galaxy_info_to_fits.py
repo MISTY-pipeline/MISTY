5ea2b5f5ed4494229b5135b58d80c9f60207b2ef
Molly Peeples <molly@stsci.edu>
2019-09-20 09:31:58 -0400

    first pass at reading in halo_info file and adding info to headers

---
+++ add_galaxy_info_to_fits.py
@@ -0,0 +1,47 @@
+'''
+
+Reads in a MISTY fits file and adds information about the galaxy to the header
+
+'''
+
+from __future__ import print_function
+
+import glob
+import os
+import argparse
+
+os.sys.path.insert(0, '/Users/molly/Dropbox/foggie/foggie')
+
+import numpy as np
+
+from astropy.io import fits
+import astropy.units as u
+from astropy.table import Table
+
+def add_galaxy_info_to_fits(filename, haloinfo):
+    hdu = fits.open(filename)
+    zsnap = hdu[0].header['REDSHIFT']
+
+    # need to find this redshift in the halo_info file
+    t = ascii.read(haloinfo, format='fixed_width')
+    thisid = t['redshift'] ==  zsnap
+    print('adding physical information from ', t[thisid])
+    assert len(t[thisid] == 1)
+
+    hdu[0].header['Mvir'] = (t['Mvir'][thisid], 'Msun')
+    hdu[0].header['Rvir'] = (t['Rvir'][thisid], 'kpc')
+    hdu[0].header['Mstar'] = (t['Mstar'][thisid], 'Msun')
+    hdu[0].header['Mism'] = (t['Mism'][thisid], 'Msun')
+    hdu[0].header['SFR'] = (t['SFR'][thisid], 'Msun/yr')
+
+
+if __name__ == "__main__":
+
+    long_dataset_list = glob.glob(os.path.join(".", 'hlsp_misty_foggie_halo008508_nref11n_nref10f*v6_los.fits.gz'))
+    haloinfo = '/Users/molly/Dropbox/foggie/foggie/halo_infos/008508/orig_nref11n/nref11n_nref10f/halo_info'
+    dataset_list = long_dataset_list
+
+    for filename in dataset_list:
+        new_filename = filename
+        print('adding galaxy info to ', filename)
+        add_galaxy_info_to_fits(filename, haloinfo)
